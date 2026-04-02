87cf3cfc51b5d88e360f9b83a4bbf7376298788e
Anindya Adhikari <135042588+ANINDYAadhikari@users.noreply.github.com>
2026-04-02 17:09:58 +0530

    Refactor PracticeFile.py for clarity and structure

--- PracticeFIle.py
+++ PracticeFIle.py
@@ -1,48 +1,48 @@
 # DAY 1 -- 30/03/2026
 
 # what is indentation ?
-'''if(5 > 2):
+if(5 > 2):
     print("five is bigger than two")
-print("hii")'''
+print("hii")
 
 
 # what is case-sensitive ?
-'''print("hiii") # THIS IS CORRECT 
-PRINT("hiii") # THIS IS WRONG '''
+print("hiii") # THIS IS CORRECT 
+PRINT("hiii") # THIS IS WRONG 
 
 
 # what is comment line ? and type of comment line ?
 # hashtag denotes single line comment 
-'''three columns denotes
- multi line comments'''
+three columns denotes
+ multi line comments
 
 
 # what is variable ?
-'''a = 5
-print(a)'''
+a = 5
+print(a)
 
 
 # How to check what type of variable ?
-'''x = 5
+x = 5
 y = "Anindya"
 z = 0.5
 print(type(x))
 print(type(y))
-print(type(z))'''
+print(type(z))
 
 
-'''x = str(5)
+x = str(5)
 print(x)
-print(type(x))'''
+print(type(x))
 
 
 # how to asssign multi values in a single var ?
-'''x, y, z = "11", "Anindya", "0.11"
-print(x, y, z)'''
+x, y, z = "11", "Anindya", "0.11"
+print(x, y, z)
 
 
 # one value to single var
-'''x = y = z = 11
-print(x, y, z)'''
+x = y = z = 11
+print(x, y, z)
 
 
